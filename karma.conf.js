26d0bfeaf368b74530690d28d2f74122a05ebd70
Turner Hayes <turnerhayes@google.com>
2020-03-26 01:05:37 -0700

    Replace remaining 'mysterybot's in the codebase

--- karma.conf.js
+++ karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/mysterybot'),
+      dir: require('path').join(__dirname, './coverage/mysteryofthethreebots'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
